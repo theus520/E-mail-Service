183db5b6e8441c5be10132b62b0e9f6d120373c5
Matheus rabelo <matheusrabelo520@gmail.com>
2024-01-09 20:43:06 -0300

    Criando a camada de adpaters na aplicação

--- EmailService/src/main/java/com/example/rabelo/application/EmailSenderService.java
+++ EmailService/src/main/java/com/example/rabelo/application/EmailSenderService.java
@@ -1,5 +1,22 @@
 package com.example.rabelo.application;
 
-public class EmailSenderService {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.rabelo.core.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase{
+
+	private final EmailSenderGateway EmailSenderGateway;
+	
+	@Autowired
+    public EmailSenderService(EmailSenderService emailGateway) {
+		this.EmailSenderGateway = emailGateway;
+	}
+	
+	@Override
+	public void sendEmail(String to, String subject, String body) {
+		
+		
+	}
 
 }
